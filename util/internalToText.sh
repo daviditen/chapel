f7fcc4c2c3a2ee0023da748f620ef76cf7c6cfa6
David Iten <diten@cray.com>
2019-01-03 12:17:34 -0600

    Update the internalToText script to handle newer, longer internal error format
    The internal error format is now DDD-BBB-EEE-LLLL where DDD is the beginning of
    the directory name, BBB is the beginning of the file name, EEE is the end of
    the file name, LLLL is the line number.

--- util/internalToText.sh
+++ util/internalToText.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 error=$1
-
-if [[ ${#error} != 7 ]] ; then
-  echo "Argument should be 7 characters long"
+#PAS-NOR-IZE-0141
+if [[ ${#error} != 16 ]] ; then
+  echo "Argument should be 16 characters long"
   exit 1
 fi
 
-filepart=`echo $error | cut -c1-3`
-linepart=`echo $error | cut -c4-7`
+dirpart=`echo $error | cut -c1-3 | tr '[:upper:]' '[:lower:]'`
+filepart1=`echo $error | cut -c5-7`
+filepart2=`echo $error | cut -c9-11`
+linepart=`echo $error | cut -c13-16`
 
-for match in `find $CHPL_HOME/compiler -iname "$filepart*.*"` ; do
+for match in `find $CHPL_HOME/compiler/$dirpart* -depth 1 -iname "$filepart1*$filepart2.*"` ; do
   sed -e "${linepart}q;d" $match | grep INT | sed -e "s;^ *;`basename $match`:$linepart: ;"
 done
